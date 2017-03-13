1f9b2b2eafcd8c2ab31ffecee19e8524d208ae76
max-bergen <maxbergen1234@gmail.com>
2017-03-13 18:42:21 +0000

    step 1 works

---
+++ step1/get-and-print.js
@@ -0,0 +1,16 @@
+
+function getAndPrintHTMLChunks (){
+  var https = require("https");
+  var requestOptions = {
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
+  };
+  https.get(requestOptions, function(response){
+    response.setEncoding("utf8");
+    response.on("data", function(data){
+      console.log("Chunk Received: ", data);
+    })
+  })
+}
+
+getAndPrintHTMLChunks();
